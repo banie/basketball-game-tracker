32e6573090965499e7e8b8d6014aecb2f216273b
Banie Setijoso <banie.setijoso@gmail.com>
2016-01-01 11:47:20 -0500

    Added FragmentUtil to contain util methods to show fragments

---
+++ presentation/src/main/java/com/android/banuu/basketballstattracker/Util/FragmentUtil.java
@@ -0,0 +1,21 @@
+package com.android.banuu.basketballstattracker.Util;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+public final class FragmentUtil {
+  private FragmentUtil() {
+  }
+
+  public static void showFragment(FragmentManager fragmentManager, int layoutId, Fragment fragment,
+      String tag) {
+    final FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+    if (!fragment.isAdded()) {
+      transaction.add(layoutId, fragment, tag);
+    }
+    transaction.show(fragment);
+    transaction.commit();
+  }
+}
