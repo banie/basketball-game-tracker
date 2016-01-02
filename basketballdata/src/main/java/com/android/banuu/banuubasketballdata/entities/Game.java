40078a7e502ceb4c08b7b7c76bfbde656b39e3a1
Banie Setijoso <banie.setijoso@gmail.com>
2016-01-02 12:15:31 -0500

    Deleting the Score model object, we can't use it like that. Also enriching the Game object, I think that's how the UI and Db are going to use the game data

--- basketballdata/src/main/java/com/android/banuu/banuubasketballdata/entities/Game.java
+++ basketballdata/src/main/java/com/android/banuu/banuubasketballdata/entities/Game.java
@@ -1,7 +1,34 @@
 package com.android.banuu.banuubasketballdata.entities;
 
-public class Game {
-  private int timeRemaining;
-  private long createdTime;
-  private Score score;
+import android.util.Pair;
+import com.android.banuu.banuubasketballdata.entities.stats.Stat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Game {
+  private int duration;
+  private long timeStarted;
+  private Map<Team, ArrayList<Stat>> teamStats;
+
+  public Game(Team teamA, Team teamB) {
+    teamStats = new HashMap<>();
+    teamStats.put(teamA, new ArrayList<Stat>());
+    teamStats.put(teamB, new ArrayList<Stat>());
+  }
+
+  public void setDuration(int duration) {
+    this.duration = duration;
+  }
+
+  public void setTimeStarted(long timeStarted) {
+    this.timeStarted = timeStarted;
+  }
+
+  public Pair<Pair<Team, Integer>, Pair<Team, Integer>> getScore() {
+    Team[] teams = new Team[2];
+    teamStats.keySet().toArray(teams);
+
+    return new Pair<>(new Pair<>(teams[0], 0), new Pair<>(teams[1], 0));
+  }
 }
