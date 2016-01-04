40d5eefc042e79cf7d339f7d138fa33ee6e513e0
Banie Setijoso <banie.setijoso@gmail.com>
2016-01-03 23:14:03 -0500

    another renaming for main_fragment, tracked_games_view sounds more relevant

--- presentation/src/main/java/com/android/banuu/basketballstattracker/main/view/GamesViewFragment.java
+++ presentation/src/main/java/com/android/banuu/basketballstattracker/main/view/GamesViewFragment.java
@@ -18,7 +18,7 @@ public class GamesViewFragment extends Fragment implements LoaderManagerProvider
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    View fragmentView = inflater.inflate(R.layout.main_fragment, container, false);
+    View fragmentView = inflater.inflate(R.layout.tracked_games_view, container, false);
     gamesViewPresenter = new GamesViewPresenter(activity, fragmentView, this);
 
     return fragmentView;
