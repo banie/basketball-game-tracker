2351e68a2bd78b75da72596839aeea9e4c8e80fb
Banie Setijoso <banie.setijoso@gmail.com>
2016-01-17 02:53:04 -0500

    Modify the res of the stat

--- presentation/src/main/java/com/android/banuu/basketballstattracker/game/view/GameFragment.java
+++ presentation/src/main/java/com/android/banuu/basketballstattracker/game/view/GameFragment.java
@@ -5,17 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.android.banuu.basketballstattracker.R;
 import com.android.banuu.basketballstattracker.widget.floatable.FlyingItemsPresenter;
 import com.android.banuu.basketballstattracker.widget.floatable.FlyingLayout;
-import com.github.clans.fab.FloatingActionButton;
 
 public class GameFragment extends Fragment implements IOnFocusListenable {
 
   @Bind(R.id.fa)
-  FloatingActionButton faButton;
+  TextView fmStat;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +32,7 @@ public class GameFragment extends Fragment implements IOnFocusListenable {
         (FlyingLayout) LayoutInflater.from(getActivity()).inflate(R.layout.flying_stat, null);
     FlyingItemsPresenter flyingItemsPresenter = new FlyingItemsPresenter(getActivity());
     int[] location = new int[2];
-    faButton.getLocationOnScreen(location);
+    fmStat.getLocationOnScreen(location);
     flyingItemsPresenter.addFlyier(flyingLayout, location[0], location[1]);
   }
 }
